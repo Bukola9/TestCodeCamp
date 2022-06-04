80006addf8f5fbeadd35ae851410bed721e3b4da
Bukola9 <adeniranbukolasamuel@gamil.com>
2022-06-04 23:35:03 +0100

    My first Java project

---
+++ Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world! this is my first project");
+        System.out.println("Testing is my passion");
+        System.out.println("I love Java");
+
+    }
+}
